9bf73197043e345f2ab6ab63b650af474cac48cc
Jason Zopf <jasonzopf@gmail.com>
2014-01-18 15:50:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewToDvd::Application.config.secret_key_base = 'e08acb8e3f01f6ec08c1cf86cdd4bb67f87eaaa23226bb9ca31600ed5985782d5e3a73f6d5fda54877ff48b43e105af399a860a400e1a815adc3c90a2ef6f3c2'
